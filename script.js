abb0c0c09bcd76fa6fcfdf3b357b1e9760c24629
DNA601 <dnavarrete6001@gmail.com>
2022-07-01 15:26:07 -0700

    Function that corresponds with the rows. Showing past present and future.

--- script.js
+++ script.js
@@ -3,3 +3,22 @@ console.log("today", today);
 
 $("#currentDay").text(today.format('MMMM Do YYYY, h:mm a'));
 //date and time on the header.
+function timeBlockColor() {
+    var hour = moment().hours();
+
+    $(".time-block").each(function() {
+        var currentHour = parseInt($(this).attr("id"));
+        if (currentHour > hour) {
+            $(this).addClass("future");
+        } else if (currentHour === hour) {
+            $(this).addClass("present");
+        } else {
+            $(this).addClass("past");
+        }
+
+    })
+};
+//Function that corresponds with the rows. Showing past present and future.
+
+timeBlockColor();
+//This makes sure the colors are returned.
