c82e6eb2d1ca8411f9f0c09cb1dd2336a28ccebc
DNA601 <dnavarrete6001@gmail.com>
2022-07-01 15:56:30 -0700

    local storage

--- script.js
+++ script.js
@@ -22,3 +22,26 @@ function timeBlockColor() {
 
 timeBlockColor();
 //This makes sure the colors are returned.
+
+
+var saveBtn = $(".saveBtn");
+saveBtn.on("click", function() {
+    var time = $(this).siblings(".hour").text();
+    //siblings element search through dom tree.
+    var n = $(this).siblings(".n").val();
+    localStorage.setItem(time, n);
+    // n is from the class button in the html doc
+});
+
+function Bomb() {
+    $(".hour").each(function() {
+        var currentHour = $(this).text();
+        var dna = localStorage.getItem(currentHour);
+        if (dna !== null)
+        //evaluation null to not true or true
+        {
+            $(this).siblings(".n").val(dna);
+        }
+    });
+}
+Bomb(); //calling bomb to store data
